83ed65afa4e39b132d174a441d3afe35192b1e02
xudecheng <dxu104@syr.edu>
2023-02-06 14:46:03 -0800

    demofinished

--- src/main/java/com/csye6225HW1/Hw1Application.java
+++ src/main/java/com/csye6225HW1/Hw1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 //@MapperScan("com.csye6225HW1.dao")
+//test
 public class Hw1Application {
     public static void main(String[] args) {
         SpringApplication.run(Hw1Application.class, args);
