c7632141b3bb381a13f9d603ed0b249e4e5bfe80
Jesse <90865804+dxu104@users.noreply.github.com>
2023-03-01 14:03:09 -0800

    HW5_Iamge

---
+++ src/main/java/com/csye6225HW1/Exceptions/ImageException/NoContentException.java
@@ -0,0 +1,9 @@
+package com.csye6225HW1.Exceptions.ImageException;
+
+public class NoContentException extends RuntimeException {
+    public NoContentException(String message){
+        super(message);
+    }
+}
+
+
--- src/main/java/com/csye6225HW1/util/ErrorMessage.java
+++ src/main/java/com/csye6225HW1/util/ErrorMessage.java
@@ -3,6 +3,7 @@ package com.csye6225HW1.util;
 public class ErrorMessage {
 
     public static final String CHANGE_OTHER_INFORMATION = "Unauthorized, Cannot change other information";
+    public static final String CHANGE_OTHER_IMAGE = "Unauthorized, Cannot change other image";
 
     // Product related
     public static final String PRODUCT_NOT_EXIST = "Product not exist";
@@ -12,6 +13,8 @@ public class ErrorMessage {
     public static final String REPEAT_SKU = "Not allowed to create/update product with same sku";
 
     public static final String NO_CONTENT ="Please enter all product's information";
+
+    public static final String IMAGE_NOT_EXIST = "IMAGE not exist";
 
 
 }
