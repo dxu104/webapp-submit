6cb836232b10dda9fb15bc1b51db9398d9992848
Jesse <90865804+dxu104@users.noreply.github.com>
2023-04-12 19:57:11 -0700

    Update SeverIsHealthyController.java

--- src/main/java/com/csye6225HW1/controller/SeverIsHealthyController.java
+++ src/main/java/com/csye6225HW1/controller/SeverIsHealthyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SeverIsHealthyController {
 
-    @GetMapping("/healthz")
+    @GetMapping("/health")
     public String heahlth(){
         return "Sever is Healthy";
     }
