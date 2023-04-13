6ff0055f3f0942c0819442c57a00b81ef1039b6b
Jesse <90865804+dxu104@users.noreply.github.com>
2023-04-13 05:08:01 -0700

    Update SeverIsHealthController.java

--- src/main/java/com/csye6225HW1/controller/SeverIsHealthController.java
+++ src/main/java/com/csye6225HW1/controller/SeverIsHealthController.java
@@ -1,15 +1,15 @@
-// package com.csye6225HW1.controller;
+package com.csye6225HW1.controller;
 
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @Slf4j
-// @RestController
-// public class SeverIsHealthController {
+@Slf4j
+@RestController
+public class SeverIsHealthController {
 
-//    @GetMapping("/health")
-//    public String health(){
-//        return "Sever is Healthy";
-//    }
-// }
+   @GetMapping("/health")
+   public String health(){
+       return "Sever is Healthy";
+   }
+}
